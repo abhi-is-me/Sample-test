6326ac385619f3d736e9c0d498097e349cd6f0e4
abhishekkumar26 <abhishek.kumar26@nagarro.com>
2024-06-13 22:12:20 +0530

    changed to Hott

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace demo_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hott", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
